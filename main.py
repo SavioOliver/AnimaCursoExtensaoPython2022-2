5b7e536917862b2c9c17953acabd6d5dfcd83c35
Savio Oliver <65372311+SavioOliver@users.noreply.github.com>
2022-10-20 01:05:01 +0000

    Aula foi ate funcao de conversao de inteiro para string

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-#hashtag para comentario em linha
+#hashtag para comentario em linha.
 '''comentario em bloco (varias linhas) 
  Aqui voce percebe que continua em comentario
  XD
 '''
 
+# para utilizar print.
 print("Hello Word")
+
+# para utilizar string.
+nome = "Saveta Porreta"
+
+# para representar valor numerico quantificavel utilizar o valor (inteiro).
+idade = 21
+
+print("Meu apelido é: "+nome)
+# para exibir a frase da idade
+# para converter inteiro para string utiliza-se a função (str)
+print("Minha idade atual é: "+str(idade))

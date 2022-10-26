eea3fea3c5d3122f4ce9716914843f2bd241e524
Savio Oliver <65372311+SavioOliver@users.noreply.github.com>
2022-10-26 23:12:48 +0000

    Aula2 dia 26/10

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-#hashtag para comentario em linha.
-'''comentario em bloco (varias linhas) 
- Aqui voce percebe que continua em comentario
- XD
-'''
-
-# para utilizar print.
-print("Hello Word")
-
-# para utilizar string.
-nome = "Saveta Porreta"
-
-# para representar valor numerico quantificavel utilizar o valor (inteiro).
-idade = 21
-
-print("Meu apelido é: "+nome)
-# para exibir a frase da idade
-# para converter inteiro para string utiliza-se a função (str)
-print("Minha idade atual é: "+str(idade))
-print(f"Minha idade atual é: {idade}\n")
-print("Minha idade é {}".format(idade))
+#Comando input() serve para solicitar para o usuario digitar algo.
+#desta forma não guarda a informação inserida.
+#input("Usuario por favor digite seu nome: ") 
+#desta forma guarda a informação inserida.
+nome = input("Usuario por favor digite seu nome: ")
+idade = int(input ("Digite sua idade: "))
+#comando de saida para exibir na tela
+print (f"Seu nome é: {nome}")
+print ("Sua idade é:{}".format(idade))
+dobro = idade * 2
+print("O dobro da idade informada é {}".format(dobro))
